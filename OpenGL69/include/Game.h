a1e3e628893e29aaae8aced3a6724ec471d2f312
Onigumo <onigumo69@outlook.com>
2021-09-23 03:44:49 +0800

    Add levels

--- OpenGL69/include/Game.h
+++ OpenGL69/include/Game.h
@@ -2,6 +2,13 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "GameLevel.h"
+
+// Initial size of the player paddle
+const glm::vec2 PLAYER_SIZE(100.0f, 20.0f);
+// Initial velocity of the player paddle
+const float PLAYER_VELOCITY(500.0f);
+
 // Game holds all game-related state and functionality.
 // Combines all game-related data into a single class for
 // easy access to each of the components and manageability.
@@ -19,6 +26,8 @@ public:
     GameState               State;
     bool                    Keys[1024];
     unsigned int            Width, Height;
+    std::vector<GameLevel>  Levels;
+    unsigned int            Level;
     // constructor/destructor
     Game(unsigned int width, unsigned int height);
     ~Game();
--- OpenGL69/src/Game.cpp
+++ OpenGL69/src/Game.cpp
@@ -1,8 +1,10 @@
 #include "Game.h"
 #include "ResourceManager.h"
 #include "SpriteRenderer.h"
+#include "GameObject.h"
 
 SpriteRenderer* Renderer;
+GameObject* Player;
 
 Game::Game(unsigned int width, unsigned int height)
     : State(GameState::GAME_ACTIVE), Keys(), Width(width), Height(height)
@@ -12,7 +14,8 @@ Game::Game(unsigned int width, unsigned int height)
 
 Game::~Game()
 {
-
+    delete Renderer;
+    delete Player;
 }
 
 void Game::Init()
@@ -27,7 +30,24 @@ void Game::Init()
     // set render-specific controls
     Renderer = new SpriteRenderer(ResourceManager::GetShader("sprite"));
     // load textures
+    ResourceManager::LoadTexture("images\\background.jpg", false, "background");
     ResourceManager::LoadTexture("images\\awesomeface.png", true, "face");
+    ResourceManager::LoadTexture("images\\block.png", false, "block");
+    ResourceManager::LoadTexture("images\\block_solid.png", false, "block_solid");
+    ResourceManager::LoadTexture("images\\paddle.png", true, "paddle");
+    // load levels
+    GameLevel one; one.Load("levels\\one.lvl", this->Width, this->Height / 2);
+    GameLevel two; two.Load("levels\\two.lvl", this->Width, this->Height / 2);
+    GameLevel three; three.Load("levels\\three.lvl", this->Width, this->Height / 2);
+    GameLevel four; four.Load("levels\\four.lvl", this->Width, this->Height / 2);
+    this->Levels.push_back(one);
+    this->Levels.push_back(two);
+    this->Levels.push_back(three);
+    this->Levels.push_back(four);
+    this->Level = 0;
+    // configure game objects
+    glm::vec2 playerPos = glm::vec2(this->Width / 2.0f - PLAYER_SIZE.x / 2.0f, this->Height - PLAYER_SIZE.y);
+    Player = new GameObject(playerPos, PLAYER_SIZE, ResourceManager::GetTexture("paddle"));
 }
 
 void Game::Update(float dt)
@@ -37,11 +57,32 @@ void Game::Update(float dt)
 
 void Game::ProcessInput(float dt)
 {
-
+    if (this->State == GameState::GAME_ACTIVE)
+    {
+        float velocity = PLAYER_VELOCITY * dt;
+        // move playerboard
+        if (this->Keys[GLFW_KEY_A])
+        {
+            if (Player->Position.x >= 0.0f)
+                Player->Position.x -= velocity;
+        }
+        if (this->Keys[GLFW_KEY_D])
+        {
+            if (Player->Position.x <= this->Width - Player->Size.x)
+                Player->Position.x += velocity;
+        }
+    }
 }
 
 void Game::Render()
 {
-    Renderer->DrawSprite(ResourceManager::GetTexture("face"),
-        glm::vec2(200.0f, 200.0f), glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+    if (this->State == GameState::GAME_ACTIVE)
+    {
+        // draw background
+        Renderer->DrawSprite(ResourceManager::GetTexture("background"), glm::vec2(0.0f, 0.0f), glm::vec2(this->Width, this->Height), 0.0f);
+        // draw level
+        this->Levels[this->Level].Draw(*Renderer);
+        // draw player
+        Player->Draw(*Renderer);
+    }
 }
