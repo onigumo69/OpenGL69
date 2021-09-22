28e9eca49f1d8e1ebe5c7a1ffda49eb13773073a
Onigumo <onigumo69@outlook.com>
2021-09-23 03:16:52 +0800

    Add sprite renderer

--- OpenGL69/src/Game.cpp
+++ OpenGL69/src/Game.cpp
@@ -1,4 +1,8 @@
 #include "Game.h"
+#include "ResourceManager.h"
+#include "SpriteRenderer.h"
+
+SpriteRenderer* Renderer;
 
 Game::Game(unsigned int width, unsigned int height)
     : State(GameState::GAME_ACTIVE), Keys(), Width(width), Height(height)
@@ -13,7 +17,17 @@ Game::~Game()
 
 void Game::Init()
 {
-
+    // load shaders
+    ResourceManager::LoadShader("shader\\SpriteVS.glsl", "shader\\SpriteFS.glsl", nullptr, "sprite");
+    // configure shaders
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(this->Width),
+        static_cast<float>(this->Height), 0.0f, -1.0f, 1.0f);
+    ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
+    ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
+    // set render-specific controls
+    Renderer = new SpriteRenderer(ResourceManager::GetShader("sprite"));
+    // load textures
+    ResourceManager::LoadTexture("images\\awesomeface.png", true, "face");
 }
 
 void Game::Update(float dt)
@@ -28,5 +42,6 @@ void Game::ProcessInput(float dt)
 
 void Game::Render()
 {
-
+    Renderer->DrawSprite(ResourceManager::GetTexture("face"),
+        glm::vec2(200.0f, 200.0f), glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 }
