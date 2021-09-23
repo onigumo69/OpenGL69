4ebea3836fbc24dab183ee98332a1f0fa4ea5c52
Onigumo <onigumo69@outlook.com>
2021-09-24 01:47:19 +0800

    Add effects

--- OpenGL69/include/Game.h
+++ OpenGL69/include/Game.h
@@ -4,15 +4,6 @@
 
 #include "GameLevel.h"
 
-// Initial size of the player paddle
-const glm::vec2 PLAYER_SIZE(100.0f, 20.0f);
-// Initial velocity of the player paddle
-const float PLAYER_VELOCITY(500.0f);
-// Initial velocity of the Ball
-const glm::vec2 INITIAL_BALL_VELOCITY(100.0f, -350.0f);
-// Radius of the ball object
-const float BALL_RADIUS = 12.5f;
-
 // Represents the current state of the game
 enum GameState {
     GAME_ACTIVE,
@@ -30,6 +21,15 @@ enum Direction {
 
 // Defines a Collision typedef that represents collision data
 typedef std::tuple<bool, Direction, glm::vec2> Collision; // <collision?, what direction?, difference vector center - closest point>
+
+// Initial size of the player paddle
+const glm::vec2 PLAYER_SIZE(100.0f, 20.0f);
+// Initial velocity of the player paddle
+const float PLAYER_VELOCITY(500.0f);
+// Initial velocity of the Ball
+const glm::vec2 INITIAL_BALL_VELOCITY(100.0f, -350.0f);
+// Radius of the ball object
+const float BALL_RADIUS = 12.5f;
 
 // Game holds all game-related state and functionality.
 // Combines all game-related data into a single class for
